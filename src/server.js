20c2d8dee3393a0df5dc6eafccff93174942325c
Andrew Dames <andrew@psych-media.com>
2018-09-24 22:37:03 -0400

    c

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ server.get('/', (req, res) => {
    * renderToString() will take our React app and turn it into a string
    * to be inserted into our Html template function.
    */
-  const body = renderToString(<App />);
+  //const body = renderToString(<App />);
   const title = 'Server side Rendering with Styled Components';
 
   res.send(
